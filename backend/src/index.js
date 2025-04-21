003e10a58a2987a270ed341150653891a91cd6af
OmarMosleh <omarmosleh90@gmail.com>
2025-04-21 10:42:15 +0200

    messages route done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js"; // don't forget '.js' in local files as we are using type of module
+import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -9,7 +10,8 @@ const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes); 
+app.use("/api/message", messageRoutes); 
 
 dotenv.config();
 
--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,7 +1,25 @@
 import mongoose from "mongoose";
 
 const messageSchema = mongoose.Schema(
-    {}
+    {
+        senderId: {
+            type: mongoose.Schema.Types.ObjectId,//This tells Mongoose that senderId will store an ObjectId, which is the unique identifier of a document in MongoDB.
+            ref:"User",// This establishes a reference (or relationship) to another collection—the "User" collection.
+            required: true,
+        },
+        receiverId: {
+            type : mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        text:{
+            type: String,
+        },
+        image:{
+            type: String,
+        }
+    },
+    {timestamps: true}
 )
 
 const Message = mongoose.model("Message", messageSchema);
