6df105a30a1f115bd59b0c77e71cb59502e741a2
OmarMosleh <omarmosleh90@gmail.com>
2025-04-24 15:25:32 +0200

    signup & login cycles completed

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -10,6 +10,8 @@ import {
   Loader2,
 } from "lucide-react";
 import { Link } from "react-router-dom";
+import AuthImagePattern from "../components/AuthImagePattern";
+import toast from "react-hot-toast";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -22,9 +24,25 @@ const SignUpPage = () => {
 
   const { signup, isSigningUp } = useAuthStore();
 
-  const validateForm = () => {};
+  const validateForm = () => {
+    if (!formData.fullName.trim()) return toast.error("Full name is required");
+    if (!formData.email.trim()) return toast.error("Full name is required");
+    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(formData.email)){
+      return toast.error(`${formData.email} is Invalid email format`);
+    };
+
+
+    if(formData.password.length < 6) return toast.error("Password must be at least 6 characters");
+    return true;
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
+
+   const success = validateForm();
+    if(success=== true){
+      signup(formData);
+    }
+
   };
 
   return (
@@ -78,7 +96,7 @@ const SignUpPage = () => {
                   type="email"
                   className={`input input-bordered w-full pl-10`}
                   placeholder="YourEmail@example.com"
-                  value={formData.fullName}
+                  value={formData.email}
                   onChange={(e) =>
                     setFormData({ ...formData, email: e.target.value })
                   }
@@ -145,6 +163,13 @@ const SignUpPage = () => {
           </div>
         </div>
       </div>
+
+      {/* right side of form */}
+
+      <AuthImagePattern
+        title="join our community"
+        subtitle="Connect with friends, share moments, and stay in touch with your loved ones "
+      />
     </div>
   );
 };
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,27 +1,62 @@
-import {create} from "zustand"
-import { axiosInstance } from "../lib/axios"
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
-export const useAuthStore = create((set)=> ({ // => ({}) syntax means returning object
- authUser:null,
- isSigningUp: false,
- isLoggingIn: false,
- isUpdatingProfile:false,
+export const useAuthStore = create((set) => ({
+  // => ({}) syntax means returning object
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
 
- isCheckingAuth: true,
+  isCheckingAuth: true,
 
- checkAuth: async() => {
-    try{
-        const res = await axiosInstance.get("auth/check");
-        set({authUser: res.data})
-    }catch(error){
-        console.log("error in check auth", error)
-        set({authUser: null})
-    }finally{
-        set({isCheckingAuth: false})
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("error in check auth", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
     }
- },
+  },
 
- signup: async (data) => {
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
 
- }
-}))
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      //can't understand this part
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+}));
