31bc0cbbea9dd4cd2faea2bde11f63e45db8fdee
OmarMosleh <omarmosleh90@gmail.com>
2025-05-06 20:33:55 +0300

    working on socket

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -16,11 +16,23 @@ const io = new Server(server, {
   },
 });
 
+// to store online users
+const userSocketMap = {}; //{userId: socketId}
+
 io.on("connection", (socket) => {
   console.log("A user is Connected", socket.id);
 
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
+
+  //io.emit() send events to all the connected clients
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -49,7 +49,6 @@ export const useAuthStore = create((set, get) => ({
       //can't understand this part
       set({ authUser: null });
       get().disconnectSocket();
-      console.log("disconnected");
       toast.success("Logged out successfully");
     } catch (error) {
       toast.error(error.response.data.message);
@@ -94,11 +93,20 @@ export const useAuthStore = create((set, get) => ({
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
 
-    const socket = io(BASE_URL);
+    const socket = io(BASE_URL, {
+      query: {
+        userId: authUser._id,
+      },
+    });
     socket.connect();
-    set({socket})
+    set({ socket });
+
+    socket.on("getOnlineUsers", (userIds)=>{
+      set({onlineUsers:userIds})
+    })
+
   },
   disconnectSocket: () => {
-    if(get().socket?.connected) get().socket.disconnect();
+    if (get().socket?.connected) get().socket.disconnect();
   },
 }));
